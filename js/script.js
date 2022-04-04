e8376b7e0f64e98d6c28d4e2a70175c1c2b70587
Oleksandr <sentator@gmail.com>
2022-04-04 15:22:38 +0300

    added class MenuCard

--- js/script.js
+++ js/script.js
@@ -62,7 +62,7 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 
     function getZero(num) {
-        if (num >=0 && num < 10) {
+        if (num >= 0 && num < 10) {
             return `0${num}`;
         } else {
             return num;
@@ -76,8 +76,8 @@ document.addEventListener('DOMContentLoaded', () => {
             minutes = timer.querySelector('#minutes'),
             seconds = timer.querySelector('#seconds'),
             timeInterval = setInterval(updateClock, 1000);
-            
-            updateClock();
+
+        updateClock();
 
         function updateClock() {
             const t = getTimeRemaining(endtime);
@@ -87,7 +87,7 @@ document.addEventListener('DOMContentLoaded', () => {
             minutes.textContent = getZero(t.minutes);
             seconds.textContent = getZero(t.seconds);
 
-            if (t.total <=0) {
+            if (t.total <= 0) {
                 clearInterval(timeInterval);
                 days.textContent = '00';
                 hours.textContent = '00';
@@ -127,8 +127,8 @@ function hideModal(modalElement) {
 function calcScrollWidth() {
     let div = document.createElement('div');
 
-    div.style.width ='50px';
-    div.style.height ='50px';
+    div.style.width = '50px';
+    div.style.height = '50px';
     div.style.overflow = 'scroll';
     div.style.opacity = 0;
     div.style.visible = 'hidden';
@@ -182,7 +182,7 @@ pepper.addEventListener('click', () => {
 function animationInit(stepQuantity, intervalPeriod) {
     const destination = 0;
     let currentPositionY = document.documentElement.scrollTop;
-    
+
     let animatedId = setTimeout(function animatedScroll() {
         if (destination <= currentPositionY) {
             window.scrollTo(0, currentPositionY);
@@ -193,3 +193,65 @@ function animationInit(stepQuantity, intervalPeriod) {
         }
     }, intervalPeriod);
 }
+
+// Menu card class
+class MenuCard {
+    constructor(src, alt, title, descr, price, parentSelector) {
+        this.src = src;
+        this.alt = alt;
+        this.title = title;
+        this.descr = descr;
+        this.price = price;
+        this.parent = document.querySelector(parentSelector);
+        this.transfer = 27;
+        this.changeToUAH();
+    }
+
+    changeToUAH() {
+        this.price = this.price * this.transfer;
+    }
+
+    render() {
+        const element = document.createElement('div');
+        element.innerHTML = `
+            <div class="menu__item">
+                <img src=${this.src} alt=${this.alt}>
+                <h3 class="menu__item-subtitle">${this.title}</h3>
+                <div class="menu__item-descr">${this.descr}</div>
+                <div class="menu__item-divider"></div>
+                <div class="menu__item-price">
+                    <div class="menu__item-cost">Цена:</div>
+                    <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
+                </div>
+            </div>
+        `;
+        this.parent.append(element);
+    }
+}
+
+new MenuCard(
+    'img/tabs/vegy.jpg',
+    'vagy',
+    'Меню "Фитнес"',
+    'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
+    9,
+    '.menu__field>.container'
+).render();
+
+new MenuCard(
+    "img/tabs/elite.jpg",
+    'elite',
+    'Меню “Премиум”',
+    'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
+    14,
+    '.menu__field>.container'
+).render();
+
+new MenuCard(
+    'img/tabs/post.jpg',
+    'post',
+    'Меню "Постное"',
+    'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.',
+    21,
+    '.menu__field>.container'
+).render();
