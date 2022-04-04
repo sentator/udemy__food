22c27d04536b3f24d898d5648b8a6fc8a0e857e6
Oleksandr <sentator@gmail.com>
2022-04-04 15:48:59 +0300

    used rest operator for classNames in class MenuCard

--- js/script.js
+++ js/script.js
@@ -196,13 +196,14 @@ function animationInit(stepQuantity, intervalPeriod) {
 
 // Menu card class
 class MenuCard {
-    constructor(src, alt, title, descr, price, parentSelector) {
+    constructor(src, alt, title, descr, price, parentSelector, ...classes) {
         this.src = src;
         this.alt = alt;
         this.title = title;
         this.descr = descr;
         this.price = price;
         this.parent = document.querySelector(parentSelector);
+        this.classes = classes;
         this.transfer = 27;
         this.changeToUAH();
     }
@@ -213,16 +214,20 @@ class MenuCard {
 
     render() {
         const element = document.createElement('div');
+        if (this.classes.length === 0) {
+            this.classes = 'menu__item';
+            element.classList.add(this.classes);
+        } else {
+            this.classes.forEach(className => element.classList.add(className));
+        }
         element.innerHTML = `
-            <div class="menu__item">
-                <img src=${this.src} alt=${this.alt}>
-                <h3 class="menu__item-subtitle">${this.title}</h3>
-                <div class="menu__item-descr">${this.descr}</div>
-                <div class="menu__item-divider"></div>
-                <div class="menu__item-price">
-                    <div class="menu__item-cost">Цена:</div>
-                    <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
-                </div>
+            <img src=${this.src} alt=${this.alt}>
+            <h3 class="menu__item-subtitle">${this.title}</h3>
+            <div class="menu__item-descr">${this.descr}</div>
+            <div class="menu__item-divider"></div>
+            <div class="menu__item-price">
+                <div class="menu__item-cost">Цена:</div>
+                <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
             </div>
         `;
         this.parent.append(element);
@@ -244,7 +249,8 @@ new MenuCard(
     'Меню “Премиум”',
     'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
     14,
-    '.menu__field>.container'
+    '.menu__field>.container',
+    'menu__item'
 ).render();
 
 new MenuCard(
@@ -253,5 +259,6 @@ new MenuCard(
     'Меню "Постное"',
     'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.',
     21,
-    '.menu__field>.container'
+    '.menu__field>.container',
+    'menu__item'
 ).render();
